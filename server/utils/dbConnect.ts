a0ce5d8bbcae3daf98bede57d877d9543a4e8559
habibollahi24 <habibollahi24@gmail.com>
2023-06-21 14:03:53 +0330

    fourth

--- server/utils/dbConnect.ts
+++ server/utils/dbConnect.ts
@@ -10,9 +10,12 @@ export default async function dbConnect() {
   if (connection.isConnected) {
     return;
   }
-  const db = await mongoose.connect(process.env.MONGO_URI!, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as ConnectOptions);
+  const db = await mongoose.connect(
+    "mongodb+srv://habibollahi24:5H6EQ3ynL4mX4w2e@cluster0.biezjwk.mongodb.net/firtsTodo",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions
+  );
   connection.isConnected = db.connections[0].readyState;
 }
