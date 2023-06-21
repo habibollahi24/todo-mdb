566b1866d1ca252159a33905dc86afe098369efb
habibollahi24 <habibollahi24@gmail.com>
2023-06-21 13:26:55 +0330

    third

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="text-center p-6 text-3xl text-secondary-300 font-semibold">
-      Todo App
+      Todo Appss
     </div>
   );
 }
